12dedd5e8a4ca0620ae694f251c7a08dd297cc5a
JoaoLCom <joao@compliasset.com>
2022-03-17 17:55:32 -0300

    add escolha de classes

--- main.rb
+++ main.rb
@@ -6,7 +6,16 @@ require "./itens.rb"
 pocao_hp = Item.new("Poção de Hp", 20, 0)
 pocao_at = Item.new("Poção de Coragem", 0, 5)
 pocao_mx = Item.new("Poção Lendaria", 100, 200)
-barbaro = Classes.new(4, 4, 2, 40, 70, 'machado')
+
+tipo_classes = []
+barbaro = Classes.new("Barbaro", 4, 4, 2, 30, 70, 'machado')
+tipo_classes.push(barbaro.nome)
+espadachim = Classes.new("Espadachim", 4, 3, 3, 60, 40, 'espada')
+tipo_classes.push(espadachim.nome)
+gatuno = Classes.new("Gatuno", 3, 3, 4, 30, 70, 'adagas')
+tipo_classes.push(gatuno.nome)
+guerreiro = Classes.new("Guerreiro", 2, 4, 4, 80, 20, 'escudo')
+tipo_classes.push(guerreiro.nome)
 
 
 puts " ___________________________________"
@@ -18,10 +27,37 @@ puts "\e[41m|___________________________________|\e[0m
 "
 puts "\e[31mVamos começar a criando seu personagem\e[0m"
 puts "\e[34mQual o nome do seu personagem? \e[0m"
-nome_entrou = gets.chomp
+nome_entrou = gets.chomp.capitalize
+
+puts "\e[34mSelecione sua classe \e[0m"
+tipo_classes.each_with_index.map { |x, i| puts "#{i+1} #{x} " }
+
+print("\e[2mNumero referente: \e[0m")
+
+classe_verifica = gets.chomp.to_i
+classe_entrou = barbaro
+puts classe_entrou
+
+    if classe_verifica == 1
+     classe_entrou = barbaro
+    elsif classe_verifica == 2
+      classe_entrou = espadachim
+    elsif classe_verifica == 3
+      classe_entrou = gatuno
+    elsif classe_verifica == 4
+        classe_entrou = guerreiro
+    else
+        while classe_verifica <= 0 || classe_verifica > 4
+        puts "Numero de classe invalido"
+        puts "\e[34mSelecione sua classe \e[0m"
+        tipo_classes.each_with_index.map { |x, i| puts "#{i+1} #{x} " }
+        print("\e[2mNumero referente: \e[0m")
+        classe_verifica = gets.chomp.to_i 
+        end
+    end
+
 
 status = 30
-
 puts "Você tem #{status} pontos de status para distruibir, escolha com cuidado!"
 
 force_entrou = 0
@@ -37,12 +73,12 @@ puts "Você ainda tem #{status} pontos de status para distruibir"
 res_entrou = 0
 if res_entrou = 0 && status > 0
 
-    puts "\e[34mQuanto de Resistencia?\e[0m"
+    puts "\e[34mQuanto de Resistência? \e[0m"
     res_verifica = gets.chomp.to_i
 
     while res_verifica < 0 || res_verifica > status
     puts "Entrada invalida"
-    puts "\e[34mQuanto de Resistencia?\e[0m"
+    puts "\e[34mQuanto de Resistência? \e[0m"
     res_verifica = gets.chomp.to_i
     
     end
@@ -76,8 +112,8 @@ end
 puts "\e[34mQual sera sua frase de efeito?   \e[0m"
 frase_entrou = gets.chomp
 
-heroi_1 = Heroi.new(nome_entrou, barbaro, force_entrou, res_entrou, vit_entrou, frase_entrou)
-                
+heroi_1 = Heroi.new(nome_entrou, classe_entrou, force_entrou, res_entrou, vit_entrou, frase_entrou)
+heroi_1.apresentar       
 
 
 puts "\e[7mPersonagem #{heroi_1.nome} criado com sucesso\e[0m"
--- personagens.rb
+++ personagens.rb
@@ -5,31 +5,38 @@ class Heroi
         @poder_arma = 0
         @defesa_armadura = 0
         @nome = nome
-        @classe = classe
-        @force = force
-        @resistencia = resistencia
-        @vitalidade = vitalidade
-        @ataque = classe.force + (@force + @poder_arma)
-        @defesa = classe.resistencia + (@resistencia + @defesa_armadura)
-        @hp = classe.hp * (@vitalidade + classe.vitalidade)
-        @vigor =classe.vigor * ((classe.resistencia + @resistencia) + (classe.force + @force))/5
+        @classe = classe.nome
+        @force = force + classe.force
+        @resistencia = resistencia + classe.resistencia 
+        @vitalidade = vitalidade + classe.vitalidade
+        @ataque = @force + @poder_arma
+        @defesa = @resistencia + @defesa_armadura
+        @hp = classe.hp * @vitalidade
+        @vigor = classe.vigor * (@resistencia + @force) /5
         @frase = frase
         @tipo_arma = classe.tipo_arma
     end
 
     def apresentar
+        puts "------Status-------"
+        puts "Nome: #{@nome}"
+        puts "Classe: #{@classe}"
+        puts "Força: #{@force}"
+        puts "Resistência: #{@resistencia}"
+        puts "Vitalidade: #{@vitalidade}"
+        puts "Ataque: #{@ataque}"
+        puts "Desefa: #{@defesa}"
+        puts "HP: #{@hp}"
+        puts "Vigor: #{@vigor}"
+
         
-         puts"\e[93m- Eu me chamo #{self.nome}, tenho uma incrivel defesa de #{self.defesa} e um poderoso ataque de #{self.ataque}, minha vida é de #{self.hp}, e um vidor de #{@vigor} e uso #{@tipo_arma} como arma será que é o suficiente para essa aventura?\e[0m"
-         
-        
-        puts "\e[36m- #{self.frase}\e[0m
-        "
     end
 end
 
 class Classes
-    attr_accessor :force, :resistencia, :vitalidade, :hp, :vigor, :tipo_arma
-    def initialize (force, resistencia, vitalidade, hp, vigor, tipo_arma)
+    attr_accessor :nome, :force, :resistencia, :vitalidade, :hp, :vigor, :tipo_arma
+    def initialize (nome, force, resistencia, vitalidade, hp, vigor, tipo_arma)
+        @nome = nome
         @force = force
         @resistencia = resistencia
         @vitalidade = vitalidade
